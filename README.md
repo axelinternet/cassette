94bb88616ab3d84a9f5936972bcbf4ab832fa3bd
axelinternet <axel@hultman.info>
2017-12-22 15:01:18 +0100

    Working basic API that listens for spotify uri and plays that corresponding song from currect active spotify device

---
+++ README.md
@@ -0,0 +1,5 @@
+
+Auth:
+https://beta.developer.spotify.com/documentation/general/guides/authorization-guide/
+Api:
+https://beta.developer.spotify.com/documentation/web-api/guides/using-connect-web-api/#viewing-active-device-list
--- play-song.js
+++ play-song.js
@@ -1,43 +1,52 @@
+require('console-warn')
+require('console-error')
+require('console-info')
+
+const http = require('http')
 const fetch = require('node-fetch')
 const request = require('request')
 const fs = require('fs')
-// Colorize errors, warn, info
-require('console-warn');
-require('console-error');
-require('console-info');
-
+const util = require('util')
+const AppData = require('./keys.secret')
 /*
- Fixat så att det går att köra en query iaf. För att starta behöver man klippa in i en ruta. Det är väl OK nu men:
+	TODO: 
+		- Spela upp en låt från tapeLibrary
+		- Fyll tapeLibrary med lite Sample Data
+		- flytta keys till secrets fil
+	Bygger hela det här som en enkel node app bara. Skickar enkla get mellan delarna så kan man dra 
+	isär modulerna sen om det visar sig vara enklare. Den här appen skulle ju kunna ligga på en server då? Då löser man inloggningen osv enkelt också. Vart kan man hosta node? Zeit now
+*/
 
- 	X Det behövs ett flöde för att ta han dom den strängen. Gör en funktion som tar den parametern man får tillbaka och sen fixar resten. Det är kanske egentligen bara att sno deras login flöde från appen och sluta krånga till det. Bara så appen kan bli authad i första steget.
+/*  
+		Sample of how to build up tapeLibrary. This object is what will be used to keep the 
+		connection between my service and spotify. Building some sort of funky interface for this
+		would be cool. Buiild this as a rest microservice and just build a super simple NUXT or next.js
+		site? 
+*/ 
 
-	- flytta keys till secrets fil
-
- 	X Lägg till korrekta scopes för spotify connect
-
- 	- Lägg still scope variabeln. 
-
- 	- Hämta ut enheter via connect
-
- 	- Bygg en mer dynamisk query funktion
-
- 	- bygg refresh token funktion
-
-Behöver också klura ut hur man ska ta imot signalen från kasetten. Det kanske bara är lättare att flytta över hela den här skiten till python? Eller eventuellt bara ta deras exempel och ha som login. Blanding av post och gets i alla fall. Ganska många olika. Annars så kan man ju bara ha ett script här eller i package.json som startar python applikationen, den kan ju skicka hit bara med en ton. Flexibelt?
-https://axelinternet.github.io/?code=
-/*/
-
-const AppData = {
-	redirect_uri: 'https://axelinternet.github.io/',
-	browserStartURL: 'https://accounts.spotify.com/authorize?client_id=8ea462bac3fa40fa9d9d5679e2b6f93d&response_type=code&redirect_uri=https://axelinternet.github.io/&scope=user-read-currently-playing%20user-modify-playback-state%20user-read-playback-state%20streaming',
-	client_id: '8ea462bac3fa40fa9d9d5679e2b6f93d',
-	client_secret: '08bbb45c21f14b73acd6b36937959ffa',
-	scope: 'user-read-currently-playing%20user-modify-playback-state%20user-read-playback-state%20streaming',
-	tokens: {}
+const generateStateKey = (keyLength = 8) => {
+	const allowed = 'abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ0123456789'
+	let key = ''
+	for (let i = 0; i < keyLength; i++) {
+		key = key + allowed[Math.round(Math.random() * allowed.length)]
+	}
+	return key
 }
 
-// Assign manually
-const code = 'AQBnfXnau3GTptV-sxRMUZBfNqUUU52rsTdIVMuh3fJGErR3yVILTiKUxj7pePS7wK6nS_Xqp42YKyZaJlBXOIRk0y4BuUmrT3WlDc6rmILthZ2HX5sv3jaiUtH721dBip4f7jJqj29pZ5Sv-1CtvvKttIEq5sYCMXRTdJmsJyUYXQLrvFYobE_CpdPrIJqdpdnUJw1yDHxC3axGqFwZCCLtYDit0IK7G0WhOqXPhAliP91Po4cT7zgO69ETrNe42j7YA6v7qkdC5Sdm40MUrp_ub0nVmt_4PZFNGswOrc-Gd17RnLPHIVUtz-r1uT4A9rF2'
+const tapeLibrary = {
+	tapes: [
+		{
+			mer_data: 'get this from API later',
+			spotify_uri: 'spotify:album:3AZThW5w8QRSZ0SRhiHARd'
+		}
+	]
+}
+
+const loadKeys = () => {
+	const tokens = JSON.parse(fs.readFileSync('keys.secret').toString())
+	// TODO: Something to check if token has expired
+	return tokens
+}
 
 const getAccessToken = (code) => {
 	// Run this first with code to get inital accesstoken. 
@@ -57,21 +66,48 @@ const getAccessToken = (code) => {
 
 	request.post(authOptions, function(error, response, body) { 
 		if (!error && response.statusCode == 200) {
-
 			console.log(body)
 			fs.writeFile('tokens.json', JSON.stringify(body), error => {error ? (console.error(error)):null})
 	  } else {
-	  	console.error(error, body, "\n", response)
+	  	console.error(error, body)
 	  } 
 	})
 	
-
 }
 
-const loadTokens = () => JSON.parse(fs.readFileSync('tokens.json').toString())
+const refreshToken = (code) => {
+	console.log(AppData.tokens)
+	// Run this first with code to get inital accesstoken. 
+	const authOptions = {
+	  url: 'https://accounts.spotify.com/api/token',
+	  form: {
+	  	refresh_token: AppData.tokens.refresh_token,
+	    grant_type: 'refresh_token'
+	  },
+	  headers: {
+	    'Authorization': 'Basic ' + (new Buffer(AppData.client_id + ':' + AppData.client_secret).toString('base64'))
+	  },
+	  json: true
+	}
 
+	request.post(authOptions, function(error, response, body) { 
+		if (!error && response.statusCode == 200) {
+			console.log(body)
+			fs.writeFile('tokens.json', JSON.stringify(body), error => {error ? (console.error(error)):null})
+	  } else {
+	  	console.error(error, body)
+	  } 
+	})
+	
+}
 
-const makeQuery = (url) =>  {
+const loadTokens = () => {
+	const tokens = JSON.parse(fs.readFileSync('tokens.json').toString())
+	// TODO: Something to check if token has expired
+	return tokens
+}
+
+const makeSimpleQuery = (url) =>  {
 	return new Promise((resolve, reject) => {
 		const query = {
 			url: url,
@@ -81,28 +117,117 @@ const makeQuery = (url) =>  {
 		}
 
 		request.get(query, function(error, response, body) {
-			console.info('Sending query')
 			if (!error && response.statusCode == 200) {
 				resolve(body)
 		  } else {
-		  	reject(error)
+		  	reject([error, response.statusCode, body])
 		  }
 		})
 	})
 }
 
-
-
-
-// Get initial token
-//getAccessToken(code)
-// Load saved tokens from file and send test request
-//loadTokens()
-
-
-AppData.tokens = loadTokens()
-makeQuery('https://api.spotify.com/v1/me/player/devices')
-	.then(data => {
-			console.log(data)
+playSong = (spotify_uri) => {
+	AppData.tokens = loadTokens()
+	makeSimpleQuery('https://api.spotify.com/v1/me/player/devices')
+		.then(data => {
+			return JSON.parse(data).devices
 		})
-		.catch(err => {console.error('ERROR', err)})
+		.then(devices => {
+			return new Promise((resolve, reject) => {
+				const query = {
+					url: 	'https://api.spotify.com/v1/me/player/play',
+					headers: {
+						'Authorization': 'Bearer ' + AppData.tokens.access_token
+					},
+					body: {
+					  "context_uri": spotify_uri
+					},
+					json: true
+				}
+
+				request.put(query, function(error, response, body) {
+					console.info('Sending query')
+					if (!error && response.statusCode == 200 || !error && response.statusCode == 204 ) {
+						console.info('Status: ', response.statusCode)
+						resolve(body)
+				  } else if (response.statusCode == 403){
+				  	resolve(body)
+				  } else {
+				  	reject([error, response.statusCode, body])
+				  }
+				})
+			})
+		})
+		.catch((err) => {
+			err.map(message => console.error(message))
+		})
+}
+
+const setup = () => {
+	/* Setup initial tokens */
+	process.stdin.resume()
+	process.stdin.setEncoding('utf8')
+	console.info('Enter this into the browser and copy the code back: ')
+	console.log(AppData.browserStartURL)
+	process.stdin.on('data', function (text) {
+	console.log('received data:', util.inspect(text))
+		AppData.code = text.replace('\n', '')
+		getAccessToken(AppData.code)
+	})
+
+}
+
+const parseParams = (url) => {
+	const requestKeys = {}
+	try {
+		let keyValues = url.split('?')[1]
+		keyValues = keyValues.split('&')
+		keyValues.forEach(key => {
+			arr = key.split('=')
+			requestKeys[arr[0]] = arr[1]
+		})
+		return requestKeys
+	} catch(e) {
+		// statements
+		console
+		.warn('No parameters provided');
+		return 
+	}
+}
+
+/*********************************************** 
+					RUN SERVER & DO THINGS 
+************************************************/ 
+
+//setup()
+//playSong(song)
+//refreshToken()
+//setup()
+const run = () => http.createServer((request, response) => {
+	const stateKey = generateStateKey()
+	console.info('StateKey', stateKey)
+  const { headers, method, url } = request
+  let body = []
+  request.on('error', (err) => {
+    console.error(err)
+  }).on('data', (chunk) => {
+    body.push(chunk)
+  }).on('end', () => {
+    response.on('error', (err) => {
+      console.error(err)
+    })
+
+    body = Buffer.concat(body).toString()
+    params = parseParams(request.url)
+    try {
+			playSong(params.spotify_uri)
+    } catch(e) {
+    	console.error("Invalid request, no spotify_uri provided");
+    }
+    response.writeHead(200, {'Content-Type': 'application/json'})
+
+    response.end(body)
+  })
+}).listen(8080)
+
+run()
